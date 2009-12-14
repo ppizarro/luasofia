65e32508964a7e9e29e884a820cb609aec28c107
katcipis <tiagokatcipis@gmail.com>
2009-12-14 16:21:34 -0200

    adding license to source files -closes #41

--- src/sip/luasofia_sip_event_private.h
+++ src/sip/luasofia_sip_event_private.h
@@ -1,4 +1,26 @@
 /* vim: set ts=8 et sw=4 sta ai cin: */
+/*
+ * Copyright (C) 2009 Digitro Corporation <www.digitro.com.br>
+ *
+ * @author Paulo Pizarro  <paulo.pizarro@gmail.com>
+ * @author Tiago Katcipis <tiagokatcipis@gmail.com>
+ *
+ * This file is part of Luasofia.
+ *
+ * Luasofia is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Luasofia is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Luasofia.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __LUASOFIA_SIP_EVENT_PRIVATE_H__
 #define __LUASOFIA_SIP_EVENT_PRIVATE_H__
 
--- test/nua.lua
+++ test/nua.lua
@@ -1,3 +1,24 @@
+--[[
+Copyright (C) 2009 Digitro Corporation <www.digitro.com.br>
+
+@author Paulo Pizarro  <paulo.pizarro@gmail.com>
+@author Tiago Katcipis <tiagokatcipis@gmail.com>
+
+This file is part of Luasofia.
+
+Luasofia is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Luasofia is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Luasofia.  If not, see <http://www.gnu.org/licenses/>.
+]]--
 
 su = require "luasofia.su"
 nua = require "luasofia.nua"
