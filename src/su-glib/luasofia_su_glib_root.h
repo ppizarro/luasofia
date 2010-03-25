db070c067c7b94ca1eae28219c4cc6af7df3a08e
Paulo Pizarro <paulo.pizarro@gmail.com>
2010-03-25 16:32:16 -0300

    Change configure.ac to check if installed lua5.1 package or lua package and change Makefile to fix errors on include paths and install path.

--- src/su-glib/luasofia_su_glib_root.h
+++ src/su-glib/luasofia_su_glib_root.h
@@ -22,8 +22,6 @@
 #ifndef __LUASOFIA_SU_GLIB_ROOT_H__
 #define __LUASOFIA_SU_GLIB_ROOT_H__
 
-#include <sofia-sip/su_wait.h>
-
 int luasofia_su_glib_root_register_meta(lua_State *L);
 
 int luasofia_su_glib_root_create(lua_State *L);
