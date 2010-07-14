9e6a1ab9aa0c9d7c70c5befb91f2245036b2d1c5
Stephanie Bastos <fepha@fepha-laptop.(none)>
2010-07-14 19:57:56 -0300

    debugging su_glib

---
+++ test/su_glib_require.lua
@@ -0,0 +1,40 @@
+function print_table(table)
+    if not table then print("nil table !!!"); return end
+    for k, v in pairs(table) do
+        print(k, v)
+    end
+end
+
+local su = require "sofia.su"
+
+print("\n<<<<<<<<<<<<<<< Required su >>>>>>>>>>>>>>>>\n")
+
+print("\nPrinting local su: ")
+print_table(su)
+
+print("\nPrinting global sofia: ")
+print_table(sofia)
+
+print("\nPrinting global su: ")
+print_table(sofia.su)
+
+
+local glib = require "sofia.glib"
+
+print("\n<<<<<<<<<<<<<<< Required glib >>>>>>>>>>>>>>>>\n")
+
+print("\nPrinting local su: ")
+print_table(su)
+
+print("\nPrinting local glib: ")
+print_table(glib)
+
+print("\nPrinting global sofia: ")
+print_table(sofia)
+
+print("\nPrinting global su: ")
+print_table(sofia.su)
+
+print("\nPrinting global glib: ")
+print_table(sofia.glib)
+
